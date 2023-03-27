19a153342cd3c82f299733c1e5a73e7c207b034f
anubhav <anubhavadhikari15@gmail.com>
2023-03-27 11:59:39 +0530

    arrow functions added

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const colors = ["green", "red", "blue", "yellow", "orange", "violet"];
 
 const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn-2');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function(){
-  console.log(document.body);
+btn2.addEventListener('click', () => {
   //get random number between 0 - 3
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
-function getRandomNumber(){
+const getRandomNumber = () => {
   //Math.random generates random number between 0-1, 
-  //multiplying length of colors array with ran.number gives val between 0-3
+  //multiplying length of colors array with ran.number gives val between 0-(len-1)
   //Math.floor rounds down decimals to nearest whole number - eg. 0.98 -> 0
   return Math.floor(Math.random() * colors.length);
-}
+};
+
+let nextNumber = 0;
+
+btn.addEventListener('click', () => {
+    nextNumber++;
+    if (nextNumber == colors.length){
+      nextNumber = 0; 
+    }
+    document.body.style.backgroundColor = colors[nextNumber];
+    color.textContent = colors[nextNumber];
+});
